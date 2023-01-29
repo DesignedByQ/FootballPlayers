b581da26c166f06b2d88edf471d2a3f6a613ac96
henry.opara <henry.opara@HYDPCM266147L>
2023-01-29 10:02:45 +0000

    added beans to main class

--- FootballPlayers/src/main/java/com/players/football/FootballApplication.java
+++ FootballPlayers/src/main/java/com/players/football/FootballApplication.java
@@ -1,7 +1,10 @@
 package com.players.football;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FootballApplication {
@@ -9,5 +12,16 @@ public class FootballApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FootballApplication.class, args);
 	}
-
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+		
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
